604092149fc7b8e0d4e18dcf1e3108f0572e83d2
Paulo Henrique Andrade Alves <80326186+PhAlves23@users.noreply.github.com>
2022-11-27 21:53:06 -0300

    :sparkles: criando estrutura do componente Header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <div>
+        {/* <img src="" alt="" /> */}
+        <h1>Logo</h1>
+      </div>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Sobre nós</a>
+            </li>
+            <li>
+              <a href="#">Serviços</a>
+            </li>
+            <li>
+              <a href="#">Contato</a>
+            </li>
+          </ul>
+        </nav>
+        <a href="">(11) 97226-6510</a>
+      </div>
+    </header>
+  );
+}
