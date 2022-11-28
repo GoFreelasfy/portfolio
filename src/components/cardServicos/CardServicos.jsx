9a7e24b9f30c4145149698ec0a294b94d9444b0c
Paulo Henrique Andrade Alves <80326186+PhAlves23@users.noreply.github.com>
2022-11-28 17:55:08 -0300

    :sparkles: :lipstick: desenvolvendo componente CardServicos

---
+++ src/components/cardServicos/CardServicos.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import styled from "styled-components";
+import arrowLeft from "../../assets/icons/arrow-left.svg";
+export default function CardServicos({ title, descricao }) {
+  const DivCardServicos = styled.div`
+    background: var(--white);
+    box-shadow: 0px 5px 30px rgba(0, 0, 0, 0.08);
+    border-radius: 10px;
+    max-width: 300px;
+    padding: 2rem;
+    display: flex;
+    cursor: pointer;
+    flex-direction: column;
+    gap: 1rem;
+    transition: 0.2s all ease-in-out;
+
+    &:hover {
+      transform: scale(1.1);
+
+      span {
+        color: var(--orange);
+      }
+
+      .arrow-left > img {
+        left: 0;
+      }
+    }
+
+    h3 {
+      font-family: "Poppins", sans-serif;
+      font-weight: 500;
+      color: var(--orange);
+      font-size: 18px;
+    }
+    p {
+      font-family: "Montserrat", sans-serif;
+      color: #787878;
+      font-size: 14px;
+    }
+    span {
+      font-family: "Poppins", sans-serif;
+      text-transform: uppercase;
+      font-weight: 700;
+      font-size: 10px;
+      transition: 0.2s all ease-in-out;
+    }
+
+    .card-footer {
+      display: flex;
+      justify-content: space-between;
+
+      .arrow-left {
+        display: flex;
+        align-items: center;
+        position: relative;
+
+        & > img {
+          position: absolute;
+          right: 5px;
+        }
+        &::after {
+          content: "";
+          display: inline-block;
+          background: var(--orange-light);
+          width: 10px;
+          height: 10px;
+          border-radius: 50%;
+        }
+      }
+    }
+  `;
+  return (
+    <DivCardServicos>
+      <h3>{title}</h3>
+      <p>{descricao}</p>
+      <div className="card-footer">
+        <span>Veja mais</span>
+        <div className="arrow-left">
+          <img src={arrowLeft} alt="Icone de seta para esquerda" />
+        </div>
+      </div>
+    </DivCardServicos>
+  );
+}
