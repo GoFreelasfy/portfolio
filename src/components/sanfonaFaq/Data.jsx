4b8c94be2fd339905b047bdb38c1ffd2e2393bd0
JhonnyO3 <jhonatas2004@gmail.com>
2022-11-30 17:04:53 -0300

    🗃️ Criado dados de FAQ

---
+++ src/components/sanfonaFaq/Data.jsx
@@ -0,0 +1,28 @@
+export const Data = [
+    {
+        pergunta: "Por que contratar a Empresa?",
+        resposta: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint in, ex reiciendis exercitationem distinctio consequuntur ipsam!"
+    },
+    {
+        pergunta: "Quanto custa um site?",
+        resposta: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint in, ex reiciendis exercitationem distinctio consequuntur ipsam!"
+    },
+    {
+        pergunta: "O pagamento pode ser parcelado?",
+        resposta: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint in, ex reiciendis exercitationem distinctio consequuntur ipsam!"
+    },
+    {
+        pergunta: "Como são as estapas de desenvolvimento?",
+        resposta: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint in, ex reiciendis exercitationem distinctio consequuntur ipsam!"
+    },
+    {
+        pergunta: "O site contará com responsividade?",
+        resposta: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint in, ex reiciendis exercitationem distinctio consequuntur ipsam!"
+    },
+    {
+        pergunta: "Por que contratar a Empresa?",
+        resposta: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint in, ex reiciendis exercitationem distinctio consequuntur ipsam!"
+    },    
+
+]
+
